fa36aee1f1bd384530b77af7506a38e239dec34f
Your Name <you@example.com>
2017-06-24 21:44:13 +0300

    Implement basic rest api.

--- api_server.py
+++ api_server.py
@@ -13,25 +13,63 @@ from model.metric_data import MetricData
 
 from config import config
 
+import json
+import datetime
 
-class UserAPI(object):
-    """User API controller"""
+
+class _JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
+        return super().default(obj)
+
+    def iterencode(self, value):
+        # Adapted from cherrypy/_cpcompat.py
+        for chunk in super().iterencode(value):
+            yield chunk.encode("utf-8")
+
+
+def json_handler(*args, **kwargs):
+    # Adapted from cherrypy/lib/jsontools.py
+    value = cherrypy.serving.request._json_inner_handler(*args, **kwargs)
+    return _JSONEncoder().iterencode(value)
+
+
+class MetricApi(object):
     @cherrypy.expose
-    def index(self):
-        return "user api"
+    @cherrypy.tools.json_out(handler=json_handler)
+    def index(self, tenant, metric):
+        db = cherrypy.request.db
+        metric = metric.replace('%2f', '/')
+        metric = metric.replace('%2F', '/')
+        rows = db.query(MetricData.timestamp, MetricData.value) \
+            .filter_by(metric_id=metric, tenant=tenant).all()
+        return rows
 
 
-class AdminAPI(object):
-    """Admin API controller"""
+@cherrypy.popargs('metric', handler=MetricApi())
+class TenantApi(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out(handler=json_handler)
+    def index(self, tenant):
+        db = cherrypy.request.db
+        rows = db.query(Metric.metric_id) \
+            .filter_by(tenant=tenant).all()
+        return [x[0] for x in rows]
+
+
+@cherrypy.popargs('tenant', handler=TenantApi())
+class UserApiRoot(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        return "admin api"
+        db = cherrypy.request.db
+        rows = db.query(Tenant.name).all()
+        return [x[0] for x in rows]
 
 
 def start():
-    cherrypy.tree.mount(UserAPI(), '/', config=config)
-    cherrypy.tree.mount(AdminAPI(), '/admin', config=config)
+    cherrypy.tree.mount(UserApiRoot(), '/', config=config)
     cherrypy.engine.signals.subscribe()
     cherrypy.engine.start()
     cherrypy.engine.block()
@@ -57,5 +95,11 @@ if __name__ == '__main__':
 
     SAEnginePlugin(cherrypy.engine, connection_string,
                    engine_args, session_args).subscribe()
+    cherrypy.tools.db = SATool()
+
+    if '/' in config:
+        config['/'].update({'tools.db.on': True})
+    else:
+        config['/'] = {'tools.db.on': True}
 
     start()
